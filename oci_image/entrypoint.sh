6207a897561641d2dc8c8bd2b010bbd272edd9d4
MalTeeez <41554385+MalTeeez@users.noreply.github.com>
2024-07-16 04:11:52 +0200

    Fix entrypoint variable insertion

--- oci_image/entrypoint.sh
+++ oci_image/entrypoint.sh
@@ -1,8 +1,8 @@
 #/usr/bin/bash
-sed -i 's/"token": "",/"token": "$BOT_TOKEN",/g' /home/discord_bot/config.json
-sed -i 's/"clientId": "",/"clientId": "$BOT_ID",/g' /home/discord_bot/config.json
-#sed -i 's/"yt_cookie": "",/"yt_cookie": "$YT_COOKIE",/g' /home/discord_bot/config.json
-sed -i 's/"mika_id": "",/"mika_id": "$OWNER_ID",/g' /home/discord_bot/config.json
+sed -i "s/\"token\": \"\",/\"token\": \"${BOT_TOKEN}\",/g" /home/discord_bot/config.json
+sed -i "s/\"clientId\": \"\",/\"clientId\": \"${BOT_ID}\",/g" /home/discord_bot/config.json
+#sed -i "s/\"yt_cookie\": \"\",/\"yt_cookie\": \"$YT_COOKIE\",/g" /home/discord_bot/config.json
+sed -i "s/\"mika_id\": \"\"/\"mika_id\": \"$OWNER_ID\"/g" /home/discord_bot/config.json
 
 echo "Starting node process with node $(node -v)"
 cd /home/discord_bot/
